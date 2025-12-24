628575095b20faed32ac70f4f12fdf7747089bc7
asad-ac <asadachau@gmail.com>
2025-12-24 14:54:49 -0500

    added .env

--- src/client.js
+++ src/client.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const URL = 'https://msonbklugaqqdugbwltu.supabase.co';
-const API_KEY = 'sb_publishable_Ddnp5B5iOML-tC7Q_RLY8Q_pO1RHFFX';
+const URL = import.meta.env.VITE_SUPABASE_URL;
+const API_KEY = import.meta.env.VITE_SUPABASE_API_KEY;
 
 export const supabase = createClient(URL, API_KEY);
